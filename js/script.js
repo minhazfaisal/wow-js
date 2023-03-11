024d7d543b3bbfbfaad29682884cf78b747ac547
Minhaz Faisal <faisalahmed01944@gmail.com>
2023-03-11 09:44:52 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+wow = new WOW(
+    {
+        boxClass:     'wow',      
+        animateClass: 'animated', 
+        offset:       10,          
+        mobile:       true,       
+        live:         true        
+    }
+)
+wow.init();
+
+
+
+
